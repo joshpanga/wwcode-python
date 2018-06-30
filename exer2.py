745a9e60efe8c52e50b2fa07196991167678434a
joshpanga <40712664+joshpanga@users.noreply.github.com>
2018-06-30 17:35:14 +0800

    Add files via upload

---
+++ exer2.py
@@ -0,0 +1,5 @@
+reward_number = input("Reward number")
+name = input("Customer's Name")
+gender = input("Gender")
+
+print("{} {}You're Succesfully Redeem the rewards!{}".format(reward_number,name,gender))
